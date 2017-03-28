4456309c5f47b0e5d553dd69ed2651c7ba6cda1b
studentuser <studentuser@D-69-91-159-141.dhcp4.washington.edu>
2017-03-28 14:03:03 -0700

    Question 1 completed

--- Homework1.sql
+++ Homework1.sql
@@ -4,21 +4,32 @@
 
 --Question 1
 --a
+
 create table EDGES(
 SOURCE int,
 DESTINATION int);
 
 --b
+
 insert into EDGES values(10, 5);
 insert into EDGES values(6, 25);
 insert into EDGES values (1, 3);
 insert into EDGES values(4, 4);
 
 --c
+
 select * from EDGES;
 
 --d
+
 select SOURCE from EDGES;
 
 --e
+
 select * from EDGES where SOURCE > DESTINATION;
+
+--f
+
+insert into EDGES values (-1, 2000);
+
+--Question 2
